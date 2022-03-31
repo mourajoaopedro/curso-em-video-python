6f471bf30e5ceed81ba4e1ec8e8994f877e1316f
João Pedro de Moura <mourajoaopedro48@gmail.com>
2022-03-31 17:29:20 -0300

    execício 013

---
+++ ex013.py
@@ -0,0 +1,8 @@
+salario = float(input('Qual o valor do seu salario :'))
+aumento = float(input('valor do aumento :'))
+novo = salario + (salario * aumento / 100)
+print('o salario e de {},com o aumento de {} ele e passara a ser de {}.'.format(salario, aumento, novo))
+
+
+
+
